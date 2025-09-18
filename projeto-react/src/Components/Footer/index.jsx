f1bd92ba2847653295699ffd40b40e00b51e81e7
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-18 16:03:57 -0300

    salvar

---
+++ projeto-react/src/Components/Footer/index.jsx
@@ -0,0 +1,18 @@
+import './styles.css'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className='footer_wrap'>
+        <div>
+            <div>
+                <strong>Contabilidade dendezeiros</strong>
+                </div> className='footer_logo'>MyShop</div>
+  )
+}
+
+
+
+
+
+

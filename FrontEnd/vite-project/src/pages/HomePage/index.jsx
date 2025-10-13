679f9eeb219e7353e45c65d8e7c08f0bc5695e2a
mariafernanda3108 <mariafernandasantos3108@gmail.com>
2025-10-13 16:45:10 -0300

    Add files via upload

---
+++ FrontEnd/vite-project/src/pages/HomePage/index.jsx
@@ -0,0 +1,17 @@
+import '.styles.css';
+
+function HomePage() {
+    return (
+        <div className="home-container">
+            <h1>Bem-vindo ao sistema de clientes</h1>
+            <p>Gerencies seus clientes de forma eficiente e pratica.</p>
+             <p>Utilize o menu para navegar entre as opcoes disponiveis.</p>
+        </div>
+    );
+}
+
+
+
+
+
+
